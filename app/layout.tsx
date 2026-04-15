1211cc9b9bd2eea1cc814744c23a0ce9fc9c83e8
Ilemar Andrade <ilemarandrade@gmail.com>
2026-04-15 18:02:49 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,12 +28,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <Script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9331970861016684"
-        crossOrigin="anonymous"
-        strategy="afterInteractive"
-      />
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9331970861016684"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} min-h-screen font-sans antialiased`}
       >
