37cd2670b08ac01c69ae69d49f948d0c9634fecb
Ilemar Andrade <ilemarandrade@gmail.com>
2026-07-09 11:16:47 -0400

    feat: :wrench: add config to unoptimizard images to work in cloudflare

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: 'export',
+  images: {
+    unoptimized: true, // Cloudflare no soporta el Image Optimization de Next en export estático
+  },
 };
 
 export default nextConfig;
